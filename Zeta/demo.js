b49b4fb8a657de69046fd5b9fede4d3476aafb34
suemi994 <541253501@qq.com>
2014-12-30 18:37:20 +0800

    use new API for Zeta,add error handler for both

--- Zeta/demo.js
+++ Zeta/demo.js
@@ -1,6 +1,9 @@
 var Zeta=require('zeta'),
     demo=Zeta.module('',[]);
 
+var data=require('./db.js'),
+    User=data.User,
+    Book=data.Book;
 demo.config('root',__dirname);
 demo.config('public',__dirname+'/../public');
 demo.l();
@@ -10,23 +13,53 @@ demo.get('/',function($scope,$render){
     $scope.res.end($render('index.html'));
 });
 demo.get('/str',function($scope){
-    $scope.res.writeHead(200,{
-        'Content-Type':'text/plain'
-    });
-    $scope.res.write('hello,world');
-    $scope.res.end();
+    $scope.send('hello,world').end();
 });
 demo.get('/json',function($scope){
-    $scope.res.writeHead(200,{
-        'Content-Type':'application/json'
+    $scope.send({msg:"Hello,World!"});
+});
+demo.get('/read',function($scope){
+    User.findOne({name:'suemi'},function(err,doc){
+        if(err) $scope.send('fail');
+        else $scope.send(doc);
     });
-    $scope.res.write(JSON.stringify({msg:"Hello,World!"}));
-    $scope.res.end();
 });
-demo.get('/read',function($scope,$db){
-
+demo.post('write',function($scope){
+    var tmp=Math.floor(Math.random()*10000);
+    Book.findOne({serial:tmp},function(err,doc){
+        if(!doc){
+            Book({serial:tmp,money:400}).save(function(err,doc){
+                if(!err) $scope.send('success');
+                else $scope.send('fail');
+            });
+        }
+        else{
+            doc.money=400;
+            doc.save(function(err,doc){
+                if(!err) $scope.send('success');
+                else $scope.send('fail');
+            });
+        }
+    });
 });
-demo.post('write',function($scope,$db){
-
+demo.h('h0',function($scope){
+    $scope.str="";
+    $scope.go('next');
 });
+demo.h('h1',function($scope){
+    $scope.str+="0";
+    $scope.go('next');
+});
+demo.h('h2',function($scope){
+    $scope.str+="1";
+    $scope.go('next');
+});
+demo.h('h3',function($scope){
+    $scope.str+="2";
+    $scope.go('next');
+});
+demo.h('h4',function($scope){
+    $scope.send($scope.str);
+});
+demo.get('/chain',['h0','h1','h2','h3','h4']);
 demo.app();
