9dd255c431971ee2b7dfab4076f404d73b16ebab
Xinyu <bevis@mail.ustc.edu.cn>
2015-01-08 00:26:46 +0800

    sort the concurrency

--- parseData.py
+++ parseData.py
@@ -14,8 +14,9 @@ def getData(fileList):
         if os.path.isfile(filePath):
             data[f] = json.loads(open(filePath, 'r').read())
             url=data[f].keys()
-            cocurrency=data[f][url[0]].keys()
-            keyList=data[f][url[0]][cocurrency[0]].keys()
+            concurrency=data[f][url[0]].keys()
+            concurrency = [str(x) for x in sorted([int(x) for x in concurrency])]
+            keyList=data[f][url[0]][concurrency[0]].keys()
             frame = data.keys()
             chunk=dict.fromkeys(frame,dict.fromkeys(url,{}))
         else:
@@ -23,28 +24,28 @@ def getData(fileList):
     for f in frame:
         for u in url:
             for k in keyList:
-                dataType=type(data[f][u][cocurrency[0]][k])
+                dataType=type(data[f][u][concurrency[0]][k])
                 if dataType==int or dataType==float:
                     tmp=[]
-                    for c in cocurrency:
+                    for c in concurrency:
                         tmp=tmp+[dataType(data[f][u][c][k])]
                     chunk[f][u][k]=tmp
                 elif dataType==dict:
-                    percent=data[f][u][cocurrency[0]][k].keys()
+                    percent=data[f][u][concurrency[0]][k].keys()
                     tmp=dict.fromkeys(percent,[])
                     for p in percent:
-                        for c in cocurrency:
+                        for c in concurrency:
                             tmp[p]=tmp[p]+[data[f][u][c][k][p]]
                     chunk[f][u][k]=tmp
                 elif dataType==list:
                     sta=['min','mean','sd','median','max']
                     tmp=dict.fromkeys(sta,[])
                     for i in range(len(sta)):
-                        for c in cocurrency:
+                        for c in concurrency:
                             s=sta[i]
                             tmp[s]=tmp[s]+[data[f][u][c][k][i]]
                     chunk[f][u][k]=tmp
-    return chunk
+    return chunk,concurrency
 
 
 
@@ -73,7 +74,7 @@ class bmData:
         """
         parse data
         """
-        self.chunk = getData(fileList)
+        self.chunk,self.concurrency = getData(fileList)
 
     def get(self, framework, url, abFeature, index=None):
         return get(self.chunk, framework, url, abFeature, index)
